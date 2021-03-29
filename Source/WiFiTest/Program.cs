9ecc770d763806a0a0018930ce4d8fe65cb6279f
TheGreatCO <thegreatco@thegreatco.com>
2021-03-29 12:53:44 -0400

    update code to loop while pulling weather

--- Source/WiFiTest/Program.cs
+++ Source/WiFiTest/Program.cs
@@ -21,7 +21,8 @@ namespace WiFiTest
 
             // instantiate and run new meadow app
             app = new WiFiTestApp();
-            await app.RunAsync().ConfigureAwait(false);
+            await app.RunAsync()
+                     .ConfigureAwait(false);
 
             Thread.Sleep(Timeout.Infinite);
         }
@@ -34,39 +35,63 @@ namespace WiFiTest
 
         public async Task RunAsync()
         {
-            if (NetworkName == null||NetworkPassword == null)
-                throw new Exception($"Please set the {NetworkName} and {NetworkPassword} before running");
+            if (NetworkName == null || NetworkPassword == null)
+                throw new Exception(
+                    $"Please set the {NetworkName} and {NetworkPassword} before running");
 
-            var rgbPwmLed = new RgbPwmLed(Device, Device.Pins.OnboardLedRed, Device.Pins.OnboardLedGreen, Device.Pins.OnboardLedBlue);
+            var rgbPwmLed = new RgbPwmLed(
+                Device,
+                Device.Pins.OnboardLedRed,
+                Device.Pins.OnboardLedGreen,
+                Device.Pins.OnboardLedBlue);
+
+
+            try
+            {
+                rgbPwmLed.SetColor(Color.Blue);
+                Console.WriteLine("Initialize WiFi adapter...");
+                await Device.InitWiFiAdapter()
+                            .ConfigureAwait(false);
+
+                Console.WriteLine("Initialized WiFi adapter, scanning for networks.");
+                foreach (var network in Device.WiFiAdapter.Scan())
+                {
+                    Console.WriteLine(
+                        $"Found {network.Ssid}, signal strength: {network.SignalDbStrength}dB");
+                }
+
+                Console.WriteLine($"Connecting to {NetworkName}...");
+                ConnectionStatus connectionStatus;
+                while ((connectionStatus =
+                            (await Device.WiFiAdapter.Connect(NetworkName, NetworkPassword)
+                                         .ConfigureAwait(false)).ConnectionStatus)
+                    != ConnectionStatus.Success)
+                {
+                    rgbPwmLed.SetColor(Color.Red);
+                    Console.WriteLine($"WiFi connect failed with {connectionStatus}");
+                    Thread.Sleep(1000);
+                    rgbPwmLed.SetColor(Color.Yellow);
+                }
+
+                Console.WriteLine("Network connected...");
+            }
+            catch (Exception ex)
+            {
+                rgbPwmLed.SetColor(Color.Red);
+                Console.WriteLine(ex);
+            }
 
             while (true)
             {
-                rgbPwmLed.SetColor(Color.Blue);
                 try
                 {
-                    Console.WriteLine("Initialize WiFi adapter...");
-                    await Device.InitWiFiAdapter().ConfigureAwait(false);
-                    Console.WriteLine("Initialized WiFi adapter, scanning for networks.");
-                    foreach (var network in Device.WiFiAdapter.Scan())
-                    {
-                        Console.WriteLine($"Found {network.Ssid}, signal strength: {network.SignalDbStrength}dB");
-                    }
-
-                    Console.WriteLine($"Connecting to {NetworkName}...");
-                    ConnectionStatus connectionStatus;
-                    while ((connectionStatus = (await Device.WiFiAdapter.Connect(NetworkName, NetworkPassword).ConfigureAwait(false)).ConnectionStatus) != ConnectionStatus.Success)
-                    {
-                        rgbPwmLed.SetColor(Color.Red);
-                        Console.WriteLine($"WiFi connect failed with {connectionStatus}");
-                        Thread.Sleep(1000);
-                        rgbPwmLed.SetColor(Color.Yellow);
-                    }
-
-                    Console.WriteLine("Network connected...");
+                    rgbPwmLed.SetColor(Color.Blue);
                     Console.WriteLine("Getting the weather...");
                     using var client = new HttpClient();
                     client.DefaultRequestHeaders.Add("User-Agent", "curl");
-                    var weather = await client.GetStringAsync("http://wttr.in/?format=%l%20%t%20%C").ConfigureAwait(false);
+                    var weather = await client.GetStringAsync("http://wttr.in/?format=%l%20%t%20%C")
+                                              .ConfigureAwait(false);
+
                     Console.WriteLine(weather);
                     rgbPwmLed.SetColor(Color.Green);
                 }
